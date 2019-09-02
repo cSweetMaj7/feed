8aaec4da446cee91a7fbcc1353496c661fe1dac0
Charles Sweet <csweet@zynga.com>
2019-09-02 15:41:39 -0700

    - GameResources, untested

---
+++ Assets/Scripts/GameResource.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameResource
+{
+    public enum ResourceType
+    {
+        Cheese,
+        Pepperoni,
+        Canadian_Bacon,
+        Pineapple
+    }
+
+    private Dictionary<ResourceType,int> resources = new Dictionary<ResourceType,int>();
+
+    public GameResource()
+    {
+        // init all resources to zero for safety
+        SetResourceQuantity(ResourceType.Cheese, 0);
+        SetResourceQuantity(ResourceType.Pepperoni, 0);
+        SetResourceQuantity(ResourceType.Canadian_Bacon, 0);
+        SetResourceQuantity(ResourceType.Pineapple, 0);
+    }
+
+    public int GetResourceQuantity(ResourceType resourceType)
+    {
+        int result = 0;
+
+        if(resources.ContainsKey(resourceType))
+        {
+            result = resources[resourceType];
+        } else
+        {
+            // init queried resources that don't exist to zero for double safety
+            SetResourceQuantity(resourceType, 0);
+        }
+
+        return result;
+    }
+
+    public void SetResourceQuantity(ResourceType resourceType, int quantity)
+    {
+        if(resources.ContainsKey(resourceType))
+        {
+            resources[resourceType] = quantity;
+        } else
+        {
+            resources.Add(resourceType, quantity);
+        }
+    }
+
+    public int ModifyResource(ResourceType resourceType, int quantity, bool sum)
+    {
+        int resourceRemainder = 0;
+        int currentQuantity = GetResourceQuantity(resourceType); // gives back zero and inits if it doesn't exist
+        if(sum)
+        {
+            // adding, only need to check for upper limits if we want them later
+            SetResourceQuantity(resourceType, currentQuantity + quantity);
+        } else {
+            // subtracting, if we reach zero then give back any remaining resources
+            resourceRemainder = quantity;
+            int curResourceVal = GetResourceQuantity(resourceType);
+            while(curResourceVal > 0)
+            {
+                resourceRemainder--;
+                SetResourceQuantity(resourceType, --curResourceVal);
+            }
+        }
+
+        return resourceRemainder;
+    }
+
+    public int allResourceSum()
+    {
+        return GetResourceQuantity(ResourceType.Cheese) + GetResourceQuantity(ResourceType.Pepperoni) + GetResourceQuantity(ResourceType.Canadian_Bacon) + GetResourceQuantity(ResourceType.Pineapple);
+    }
+
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,16 +4,61 @@ using UnityEngine;
 
 public class Target : Barrier
 {
+    
+    public int cheese;
+    public int pepperoni;
+    public int canadianBacon;
+    public int pineapple;
+    // these are the resource amounts needed to destroy target
+    public GameResource breakResources;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // move the inspector set amounts into the GameResource
+        breakResources = new GameResource();
+        breakResources.ModifyResource(GameResource.ResourceType.Cheese, cheese, true);
+        breakResources.ModifyResource(GameResource.ResourceType.Pepperoni, pepperoni, true);
+        breakResources.ModifyResource(GameResource.ResourceType.Cheese, canadianBacon, true);
+        breakResources.ModifyResource(GameResource.ResourceType.Cheese, pineapple, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(breakResources.allResourceSum() < 1)
+        {
+            // target destroyed
+            Destroy(gameObject);
+        }
+    }
+
+    public void deductResources(GameResource ballResources)
+    {
+        //breakResources.SetResourceQuantity(GameResource.ResourceType.Cheese, ballResources.ModifyResource(GameResource.ResourceType.Cheese, breakResources)
+        ballResources.SetResourceQuantity(
+            GameResource.ResourceType.Cheese,
+            breakResources.ModifyResource(
+                   GameResource.ResourceType.Cheese, ballResources.GetResourceQuantity(GameResource.ResourceType.Cheese
+            ), false));
+
+        ballResources.SetResourceQuantity(
+            GameResource.ResourceType.Pepperoni,
+            breakResources.ModifyResource(
+                   GameResource.ResourceType.Pepperoni, ballResources.GetResourceQuantity(GameResource.ResourceType.Pepperoni
+            ), false));
+
+        ballResources.SetResourceQuantity(
+            GameResource.ResourceType.Canadian_Bacon,
+            breakResources.ModifyResource(
+                   GameResource.ResourceType.Canadian_Bacon, ballResources.GetResourceQuantity(GameResource.ResourceType.Canadian_Bacon
+            ), false));
+
+        ballResources.SetResourceQuantity(
+            GameResource.ResourceType.Pineapple,
+            breakResources.ModifyResource(
+                   GameResource.ResourceType.Pineapple, ballResources.GetResourceQuantity(GameResource.ResourceType.Pineapple
+            ), false));
     }
 
     override public BarrierType getBarrierType()
